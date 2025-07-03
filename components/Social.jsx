a0d268ed733c5b8abcf868cb06e7e7e92f873b79
sharif khan <khnasharif971@gmail.com>
2025-07-03 15:16:43 +0530

    Update Social.jsx

--- components/Social.jsx
+++ components/Social.jsx
@@ -6,7 +6,7 @@ export default function Social() {
         <div className="flex gap-2 items-center -ml-1 bg-secondary rounded-full p-2 w-fit">
             <Link href="https://github.com/sharifkhan232" className="hover:text-primary"><GitHubLogoIcon className="h-4 w-4" /></Link>
             <Link href="https://www.instagram.com/sharrriiffff?igsh=ZHF3ZGx2eDNtaXVm&utm_source=qr" className="hover:text-primary"><InstagramLogoIcon className="h-4 w-4" /></Link>
-            <Link href="https://x.com/shariifkhan?s=21" className="hover:text-primary"><TwitterLogoIcon className="h-4 w-4" /></Link>
+            <Link href="https://x.com/ShariifKhan30" className="hover:text-primary"><TwitterLogoIcon className="h-4 w-4" /></Link>
         </div>
     )
 }
